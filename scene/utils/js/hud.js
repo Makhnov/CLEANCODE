97341a37f4a0845cb2313193b07ffd2ca6c38016
Makhnov <makh@tutanota.com>
2023-02-08 19:02:19 +0100

    portfolio bouquin v2.0

--- scene/utils/js/hud.js
+++ scene/utils/js/hud.js
@@ -1,4 +1,4 @@
-const li = document.getElementsByTagName('li');
+const li = document.querySelectorAll('li.gauche');
 const navGauche = document.getElementsByTagName('nav')[0];
 const navDroite = document.getElementsByTagName('nav')[1];
 const scene = document.getElementById('containerView').children;
@@ -11,20 +11,20 @@ function loadingGIF() {
     waitingMenu.style.backgroundImage = `url(${urlGIF + new Date().getTime()})`;
 }
 
+let antiSpamm;
+let resizeSpamm;
+
 let angle = 0;
 let decal = 0;
 let depth = 50;
 let iDeg = 20;
 let iTemp = 0;
-let timer123;
 
-
-tabNav = [false, false, false, false, false];
+let tabNav = [false, false, false, false, false];
 
 window.onresize = function () {
-    let resizeTimer;
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function () {
+    clearTimeout(resizeSpamm);
+    resizeSpamm = setTimeout(function () {
         resize();
     }, 500);
 };
@@ -62,57 +62,58 @@ function resize() {
     }
 
     document.documentElement.style.setProperty('--ignitionDegree', iDeg +'deg');
+    //let iDegree = getComputedStyle(document.documentElement).getPropertyValue('--ignitionDegree');
+    //console.log(iDegree);
     document.documentElement.style.setProperty('--depthBar', depth +'px');
+    //let depthBar = getComputedStyle(document.documentElement).getPropertyValue('--depthBar');
+    //console.log(depthBar);
+
     navGauche.style.transform = "rotateY("+angle+"deg) translate3d("+decal+"px, 0, 0)";
     waitingMenu.style.width = depth+"px";
     waitingMenu.style.transform = "translate3d(0, 0, -"+depth+"px) rotateY("+-angle+"deg)";
-    let iDegree = getComputedStyle(document.documentElement).getPropertyValue('--ignitionDegree');
-    console.log(iDegree);
 }
 
 for (let i = 0; i < li.length; i++) {
     let iTemp = i;
     li[i].addEventListener('click', function () {
-        clearTimeout(timer123);
+        clearTimeout(antiSpamm);
         menuAsync(iTemp);
     });
 }
 
-async function menuAsync(iTemp){
+async function menuAsync(iTemp) {
     // BEFORE
     li[iTemp].classList.add("clicked");
     tabNav[iTemp] = true;
-
 
     let menuHeight = waitingMenu.clientWidth;
     let loadingPosY = (25 * iTemp) + (2 - iTemp) * 1.25;
     waitingMenu.style.backgroundPositionX = "center";
     waitingMenu.style.backgroundPositionY = loadingPosY+"%";
     waitingMenu.style.backgroundSize = menuHeight+"px "+menuHeight+"px";
+
     loadingGIF();
 
-    //let waitingZone = scene[iTemp]
     for (j = 0; j < li.length; j++) {
         if (li[j].classList.contains("clicked") && j !== iTemp) {
             li[j].classList.remove("clicked"); 
             tabNav[j] = false;
-            document.getElementById("exit"+j).checked = true;
-            console.log(document.getElementById("exit"+j));
         }
     }
-    console.log("BEFORE")
+
+    console.log("BEFORE :" + iTemp);
 
     //IN-BETWEEN
     await delayLi(iTemp);
     
     // AFTER
-    console.log("AFTER")
+    console.log("AFTER :" + tabNav);
 }
 
-function delayLi(){
+function delayLi() {
     return new Promise((resolve,reject)=>{
         //here our function should be implemented 
-        timer123 = setTimeout(()=>{
+        antiSpamm = setTimeout(()=>{
                 for (j = 0; j < tabNav.length; j++) {
                     if (!tabNav[j]) {
                         scene[j].classList.add("hidden");
@@ -125,28 +126,33 @@ function delayLi(){
                 waitingMenu.style.backgroundImage = "none";
                 blurBG.style.display = "none";
                 navDroite.style.display = "grid";
+                menuDroite();
                 resolve();
             ;} , speed2
         );
     });
 }
 
-function open(iTemp) {
-    switch (iTemp) {
-        case 0:
+function checkTabNav() {
+    for (let i = 0; i < 5; i++) {
+        if (tabNav[i]) {
+            return i;
+        }
+    }
+}
 
+function menuDroite() {
+    let num = checkTabNav();
+    switch (num) {
+        case 0:
             break;
         case 1:
-
             break;
         case 2:
-
             break;
         case 3:
-
             break;
         case 4:
-
             break;
     }
 }
