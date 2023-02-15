22eb42b5e63ba901afa3a656ad6b9f31397c4755
Makhnov <makh@tutanota.com>
2023-02-16 00:23:44 +0100

    yuhoumail

---
+++ utils/config.php
@@ -0,0 +1,3 @@
+<?php
+    $passwordMail = "YD4LsThNGNqAVR6//*";
+?>
---
+++ utils/sendmail.php
@@ -0,0 +1,37 @@
+<?php
+// sendmail.php
+use PHPMailer\PHPMailer\PHPMailer;
+require_once('config.php');
+require 'vendor/autoload.php';
+
+// Vérifie si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Récupère les données du formulaire
+    $nom = htmlspecialchars($_POST['nom']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Envoi du mail avec PHPMailer
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 2;
+    $mail->Host = 'smtp.hostinger.fr';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'nico@makh.fr';
+    $mail->Password = $passwordMail;
+    $mail->setFrom($email, $nom);
+    $mail->addReplyTo($email, $nom);
+    $mail->addAddress('montauriol@tutanota.com', 'Nicolas');
+    $mail->Subject = 'Formulaire de contact';
+    $mail->msgHTML($message, __DIR__);
+    $mail->Body = $message;
+    //$mail->addAttachment('test.txt');
+    if (!$mail->send()) {
+        echo 'Erreur de Mailer : ' . $mail->ErrorInfo;
+    } else {
+        echo 'Le message a été envoyé.';
+    }
+}
+?>
