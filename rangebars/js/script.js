3638729862596d446abeead9265c77fd4236fc12
Makhnov <makh@tutanota.com>
2023-02-25 10:21:44 +0100

    typo

--- rangebars/js/script.js
+++ rangebars/js/script.js
@@ -1,4 +1,4 @@
-let space =" ";
+let space = " ";
 let br = "\n";
 
 let classe;
@@ -10,70 +10,70 @@ let h1Item = document.getElementById('titre');
 
 
 for (i = 0; i < Array.from(inputItem).length; i++) {
-    labelItem[i].innerHTML = inputItem[i].name;
+	labelItem[i].innerHTML = inputItem[i].name;
 }
 
-function modif (n, x) {
-    styling = style(n);
-    h1Item.style[styling] = value(n, x);
+function modif(n, x) {
+	styling = style(n);
+	h1Item.style[styling] = value(n, x);
 }
 
-function value (n, x) {
-    switch (n) {
-        case "background" :
-            valeur = "hsl("+(3.6 * x)+", 100%, 50%)";
-            break;
-        case "padding" :
-            valeur = (0.05 * x)+"rem";
-            break;
-        case "height" :
-            valeur = (5 + (x / 2))+"vh";
-            break;
-        case "width" :
-            valeur = (5 + (x / 2))+"vw";
-            break;
-        case "radius" :
-            valeur = (x / 2)+"%";
-            break;
-        case "rotate" :
-            valeur = "rotateZ("+(-180 + (3.6 * x))+"deg)";
-            break;
-    }
+function value(n, x) {
+	switch (n) {
+		case "background":
+			valeur = "hsl(" + (3.6 * x) + ", 100%, 50%)";
+			break;
+		case "padding":
+			valeur = (0.05 * x) + "rem";
+			break;
+		case "height":
+			valeur = (5 + (x / 2)) + "vh";
+			break;
+		case "width":
+			valeur = (5 + (x / 2)) + "vw";
+			break;
+		case "radius":
+			valeur = (x / 2) + "%";
+			break;
+		case "rotate":
+			valeur = "rotateZ(" + (-180 + (3.6 * x)) + "deg)";
+			break;
+	}
 
-    console.log(valeur);
-    return valeur;
+	console.log(valeur);
+	return valeur;
 }
 
 function style(n) {
-    switch (n) {
-        case "background" :
-            type = "backgroundColor";
-            break;
-        case "padding" :
-            type = "padding";
-            break;
-        case "height" :
-            type = "height";
-            break;
-        case "width" :
-            type = "width";
-            break;
-        case "radius" :
-            type = "borderRadius";
-            break;
-        case "rotate" :
-            type = "transform";
-            break;
-    }
+	switch (n) {
+		case "background":
+			type = "backgroundColor";
+			break;
+		case "padding":
+			type = "padding";
+			break;
+		case "height":
+			type = "height";
+			break;
+		case "width":
+			type = "width";
+			break;
+		case "radius":
+			type = "borderRadius";
+			break;
+		case "rotate":
+			type = "transform";
+			break;
+	}
 
-    console.log(type);
-    return type;
+	console.log(type);
+	return type;
 }
 
 Array.from(inputItem).forEach(input => {
-    input.addEventListener("change", function() {
-    modif(input.name, input.value);
-    });
+	input.addEventListener("input", function () {
+		modif(input.name, input.value);
+	});
 });
 
 
